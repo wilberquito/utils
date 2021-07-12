c02b5ff81efba8068929531e6792c6cb9fa4d936
wilberquito <wbq.software@gmail.com>
2021-07-12 23:56:15 +0200

    uso de las funciones fold

--- recursion.hs
+++ recursion.hs
@@ -71,3 +71,57 @@ reverse' [] = []
 reverse' (x:xs) = reverse' xs ++ [x]
 
 
+
+{-
+>>> maximum'' [1,2,3,4,5,6,10,1]
+10
+>>> maximum'' [1]
+1
+-}
+maximum'' :: Ord a => [a] -> a
+maximum'' [] = error "wtf"
+maximum'' [x] = x
+maximum'' ls = foldl1 (\bef now -> if now > bef then now else bef) ls
+
+{-
+>>> reverse'' [1,2,3,4]
+[4,3,2,1]
+-}
+reverse'' :: [a] -> [a]
+reverse'' ls = foldl (\bef now -> now : bef) [] ls
+
+{-
+>>> product' [1,2,3,4]
+24
+-}
+product' :: (Num a) => [a] -> a
+product' = foldl (*) 1
+
+{-
+>>> filter' [1,1,1,3,4,2,2,5,8,5] 5
+[5,5]
+-}
+filter' :: Eq a => [a] -> a -> [a]
+filter' ls el = foldl (\x y -> if y == el then y:x else x) [] ls
+
+
+{-
+>>> head' [1,2,3]
+1
+>>> head' []
+there is no head in an empty list!
+-}
+head' :: [a] -> a
+head' [] = error "there is no head in an empty list!"
+head' ls = foldl1 (\x _ -> x) ls 
+
+
+{-
+>>> last' [1,2,3]
+3
+>>> last' []
+there is no last in an empty list!
+-}
+last' :: [a] -> a
+last' [] = error "there is no last in an empty list!"
+last' ls = foldr1 (\_ x -> x) ls 
