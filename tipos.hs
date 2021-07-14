c20df042947e0bed4463d5397d0bc724a18bb0f1
wilberquito <wbq.software@gmail.com>
2021-07-14 21:43:51 +0200

    uso del constructor de tipo newtype y el contructor de tipo data

--- tipos.hs
+++ tipos.hs
@@ -27,15 +27,25 @@ ahora bien, definir un tipo de esta manera es inutil,
 necesitamos poder crear las figuras con valores iniciales
 -}
 
-data RealShape = RealCircle Float Float Float | RealRectangle (Float, Float) (Float, Float) (Float, Float) (Float, Float)
+{-
+    new type te permite tener un constructor de tipo sobre un tipo ya existente
+    en tiempo de ejecucion sigue siendo el tipo (Float, Float) però en compilaciñon
+    el compilador vigila no confundirlo con una tupla de floats
+-}
+newtype Point2d = Point2d (Float, Float) deriving (Show)
+
+data Shape2d = 
+        Circle2d Point2d Float 
+    |   Rectangle2d Point2d Point2d Point2d Point2d
+    deriving (Show) -- de esta forma se puede representar el tipo como cadena
 
 {-
->>> :t RealCircle
-RealCircle :: Float -> Float -> Float -> RealShape
->>> :t RealRectangle
-RealRectangle :: Float -> Float -> Float -> Float -> RealShape
+>>> :t Circle2d
+Circle2d :: Float -> Float -> Float -> Shape2d
+>>> :t Rectangle2d
+Rectangle2d :: Float -> Float -> Float -> Float -> Shape2d
 
-como se puede apreciar, tanto realcircle como realrectangle són constructuroes
+como se puede apreciar, tanto realcircle como Rectangle2d són constructuroes
 del tipo realshape, otra forma de mirarlo es que son funciones que al final dan 
 el tipo realshape
 -}
@@ -43,25 +53,25 @@ el tipo realshape
 {-
     <!--
         Problema:
-            crea una función que dado una RealShape cálcule el area
+            crea una función que dado una Shape2d cálcule el area
             y otra función para el cálculo del volumen
     -->
 -}
 
 {-
->>> area (RealRectangle (0,2) (1,2) (0,0) (1,0))
+>>> area (Rectangle2d (0,2) (1,2) (0,0) (1,0))
 2.0
->>> area (RealRectangle (0,2) (2,2) (0,0) (2,0))
+>>> area (Rectangle2d (0,2) (2,2) (0,0) (2,0))
 4.0
->>> area (RealRectangle (-2,2) (0,2) (-2,0) (0,0))
+>>> area (Rectangle2d (-2,2) (0,2) (-2,0) (0,0))
 4.0
->>> area (RealRectangle (-1,2) (1,2) (-1,0) (1,0))
+>>> area (Rectangle2d (-1,2) (1,2) (-1,0) (1,0))
 4.0
 
->>> area (RealCircle 1 1 2)
+>>> area (Circle2d 1 1 2)
 12.566371
 -}
 
-area :: RealShape -> Float
-area (RealCircle _ _ r) = pi*r*r
-area (RealRectangle tl tr bl _) = abs(fst tr - fst tl) * abs(snd tl - snd bl)
+area :: Shape2d -> Float
+area (Circle2d _ r) = pi*r*r
+area (Rectangle2d (Point2d tl) (Point2d tr) (Point2d bl) _) = abs(fst tr - fst tl) * abs(snd tl - snd bl)
